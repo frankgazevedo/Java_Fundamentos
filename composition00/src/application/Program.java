076cdd2a2fa1cf4236771828fc7e03d97b0fd33c
Frank Azevedo <frankgazevedo@gmail.com>
2023-06-09 09:59:01 -0400

    composition: oop e list

--- composition00/src/application/Program.java
+++ composition00/src/application/Program.java
@@ -57,17 +57,19 @@ public class Program {
 
 		List<Employee> list = new ArrayList<>();
 
+		// PART 1 - READING DATA:
+
 		System.out.print("How many employees will be registered? ");
-		int N = sc.nextInt();
+		int n = sc.nextInt();
 
-		for (int i = 0; i < N; i++) {
-
+		for (int i = 1; i <= n; i++) {
 			System.out.println();
-			System.out.println("Emplyoee #" + (i + 1) + ":");
+			System.out.println("Employee #" + i + ": ");
+
 			System.out.print("Id: ");
-			Integer id = sc.nextInt();
+			int id = sc.nextInt();
 			while (hasId(list, id)) {
-				System.out.println("Id already taken! Try again: ");
+				System.out.print("Id already taken. Try again: ");
 				id = sc.nextInt();
 			}
 
@@ -75,52 +77,37 @@ public class Program {
 			sc.nextLine();
 			String name = sc.nextLine();
 			System.out.print("Salary: ");
-			Double salary = sc.nextDouble();
-
-			Employee emp = new Employee(id, name, salary);
-
-			list.add(emp);
+			double salary = sc.nextDouble();
+			list.add(new Employee(id, name, salary));
 		}
 
+		// PART 2 - UPDATING SALARY OF GIVEN EMPLOYEE:
+
 		System.out.println();
-		System.out.print("Enter the employee id that will have salary increase : ");
-		int idsalary = sc.nextInt();
-
-		// Integer pos = position(list, idsalary);
-
-		Employee emp = list.stream().filter(x -> x.getId() == idsalary).findFirst().orElse(null);
-
-		// if (pos == null) {
+		System.out.print("Enter the employee id that will have salary increase: ");
+		int id = sc.nextInt();
+		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
 		if (emp == null) {
 			System.out.println("This id does not exist!");
 		} else {
 			System.out.print("Enter the percentage: ");
-			double percent = sc.nextDouble();
-			// list.get(pos).increaseSalary(percent);
-			emp.increaseSalary(percent);
+			double percentage = sc.nextDouble();
+			emp.increaseSalary(percentage);
 		}
+
+		// PART 3 - LISTING EMPLOYEES:
 
 		System.out.println();
 		System.out.println("List of employees:");
-		for (Employee e : list) {
-			System.out.println(e);
+		for (Employee obj : list) {
+			System.out.println(obj);
 		}
 
 		sc.close();
-	}
-
-	public static Integer position(List<Employee> list, int id) {
-		for (int i = 0; i < list.size(); i++) {
-			if (list.get(i).getId() == id) {
-				return i;
-			}
-		}
-		return null;
 	}
 
 	public static boolean hasId(List<Employee> list, int id) {
 		Employee emp = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
 		return emp != null;
 	}
-
 }
