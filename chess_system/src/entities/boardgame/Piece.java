f6a94eead888767ba7394a5e571c9d9d9a0642a3
Frank Azevedo <frankgazevedo@gmail.com>
2023-09-04 18:59:57 -0400

    creating: class Piece

---
+++ chess_system/src/entities/boardgame/Piece.java
@@ -0,0 +1,18 @@
+package entities.boardgame;
+
+public class Piece {
+
+	protected Position position;
+	private Board board;
+
+	public Piece(Board board) {
+		super();
+		this.board = board;
+		this.position = null;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
+}
