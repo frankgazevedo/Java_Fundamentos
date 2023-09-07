ae797163b34ad88b969b59a1da48039a0ab6f47d
Frank Azevedo <frankgazevedo@gmail.com>
2023-09-07 09:52:51 -0400

    interfaces: first view: updating interface BrazilTaxService

--- interface01/src/model/services/BrazilTaxService.java
+++ interface01/src/model/services/BrazilTaxService.java
@@ -2,10 +2,13 @@ package model.services;
 
 public class BrazilTaxService implements TaxService {
 
+	private static final double TAX_PERCENTAGE_UP_TO_100 = 0.2;
+	private static final double TAX_PERCENTAGE_AFTER_100 = 0.15;
+
 	public double tax(double amount) {
 		if (amount <= 100.0)
-			return amount * 0.2;
+			return amount * TAX_PERCENTAGE_UP_TO_100;
 		else
-			return amount * 0.15;
+			return amount * TAX_PERCENTAGE_AFTER_100;
 	}
 }
